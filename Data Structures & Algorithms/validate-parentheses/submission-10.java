047cda38b807b7f7fd174bd6227df08e5409f577
JBoasman <joshboasman@gmail.com>
2026-06-06 10:54:34 +1000

    Add: validate-parentheses - submission-10

---
+++ Data Structures & Algorithms/validate-parentheses/submission-10.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isValid(String s) {
+        Deque<Character> history = new ArrayDeque<>();
+        for (int i = 0; i < s.length(); i++) {
+            switch (s.charAt(i)) {
+                case '{' -> history.push('{');
+                case '(' -> history.push('(');
+                case '[' -> history.push('[');
+                case '}' -> { if (history.isEmpty() || history.pop() != '{') { return false; } }
+                case ')' -> { if (history.isEmpty() || history.pop() != '(') { return false; } }
+                case ']' -> { if (history.isEmpty() || history.pop() != '[') { return false; } }
+                default -> {} //do nothing
+            }
+        }
+        return history.isEmpty();
+    }
+}
