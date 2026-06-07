fe409ae95f6a2dee449b28ea5e4776fec0a7b73c
JBoasman <joshboasman@gmail.com>
2026-06-07 16:05:46 +1000

    Add: search-2d-matrix - submission-5

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-5.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int width = matrix[0].length;
+        int height = matrix.length;
+        int min = 0;
+        int max = (width * height) - 1;
+        
+        while (min <= max) {
+            int mid = min + ((max - min) / 2);
+            int midValue = valueAtIndex(matrix, mid, width);
+            if ( midValue == target) {
+                return true;
+            } else if (midValue < target) {
+                min = mid + 1;
+            } else {
+                max = mid - 1;
+            }
+        }
+        return false;
+    }
+
+    public int valueAtIndex(int[][] matrix, int z, int width) {
+        int x = z % width;
+        int y = z / width;
+        return matrix[y][x];
+    }
+}
