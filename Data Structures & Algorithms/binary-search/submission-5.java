46e213bf2ad9c0ff1e9901ad1080df933a368a98
JBoasman <joshboasman@gmail.com>
2026-06-07 10:45:02 +1000

    Add: binary-search - submission-5

---
+++ Data Structures & Algorithms/binary-search/submission-5.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int min = 0;
+        int max = nums.length - 1;
+        while (min <= max) { //can point at the same element, only stops when min passes max
+            int mid = (max + min) / 2; //finds mid point
+            if (nums[mid] == target) { //if mid is poionting at at target, return index
+                return mid;
+            } else if (nums[mid] > target) { //elses for min/max
+                max = mid - 1;
+            } else {
+                min = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
